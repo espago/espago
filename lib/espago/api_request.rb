f56bef20cde8a3afc8d27ee6892644cff2a8c5dd
Piotrek <pkurek90@gmail.com>
2013-01-17 10:27:09 +0100

    added faraday to api_request class

--- lib/espago/api_request.rb
+++ lib/espago/api_request.rb
@@ -1,7 +1,22 @@
+require "faraday"
+require "base64"
+
 module Espago
   class ApiRequest
-    def create(method, params)
 
+    def initialize
+      @connection = Faraday.new("https://edge.espago.com/api")
+      @connection.headers = {'Authorization' => "Basic #{Base64.encode64("908112077")}"}
+    end
+
+    def create(path, method, params = {} )
+      klass = path.to_s.capitalize
+      binding.pry
+    end
+
+    class GetNewClient
     end
   end
 end
+
+Espago::ApiRequest.new.create(:new_client, :get )
