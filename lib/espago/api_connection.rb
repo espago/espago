47517a7ff57d56d5372538985fee099757f38b3e
Piotrek <pkurek90@gmail.com>
2013-05-28 10:24:11 +0200

    added Espago::ApiConnection error handling

--- lib/espago/api_connection.rb
+++ lib/espago/api_connection.rb
@@ -11,6 +11,9 @@ module Espago
     extend Forwardable
     def_delegator :@connection, :basic_auth, :authenticate
 
+    Error = Class.new(StandardError)
+    AuthenticationError = Class.new(StandardError)
+
     def initialize(enviroment)
       @connection = Faraday.new(enviroment)
       @router = Router
@@ -19,10 +22,31 @@ module Espago
     def create(path, method, params = {} )
       route = @router.new(path, method).route
       response = route.new(@connection).request(params)
-      parse response.body
+
+      handle_response response
     end
 
     private
+
+    def handle_response(response)
+      case response.status
+      when 200
+        return parse(response.body)
+      when 202
+        return true
+      when 400
+        raise Error, { error_message: 'bad request', status: 400 }
+      when 401
+        raise AuthenticationError, { error_message: 'not authenticated', status: 401 }
+      when 404
+        raise Error, { error_message: 'not found', status: 404 } 
+      when 407
+        raise Error, { error_message: 'proxy authentication required', status: 407 } 
+      else
+        raise Error, { error_message: 'unknown error', status: response.status }
+      end
+    end
+
     def parse(body)
       JSON.parse body
     end
