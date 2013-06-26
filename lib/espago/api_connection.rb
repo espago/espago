7d85a8060101900f2a41bb3aeb902f550be40910
Waclaw Luczak <waclaw@luczak.it>
2013-06-26 17:04:43 +0200

    Update api_connection.rb
    201...

--- lib/espago/api_connection.rb
+++ lib/espago/api_connection.rb
@@ -30,7 +30,7 @@ module Espago
 
     def handle_response(response)
       case response.status
-      when 200
+      when 200,201
         return parse(response.body)
       when 202,204
         return true
