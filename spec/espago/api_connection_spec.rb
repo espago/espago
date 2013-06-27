cf9d6b052804b38622b3c037c985dfe60a51674c
Szymon Fiedler <fidelski@gmail.com>
2013-06-27 15:13:06 +0200

    Fixed uninitialized constant issue with errors

--- spec/espago/api_connection_spec.rb
+++ spec/espago/api_connection_spec.rb
@@ -12,12 +12,24 @@ class Espago::ApiConnection::StubbedPath
   end
 end
 
+class Espago::ApiConnection::UnauthorizedPath
+  def initialize(connection)
+  end
+
+  def request(params = {})
+    StubbedResponse.new(nil, 401)
+  end
+end
+
 describe Espago::ApiConnection do
   subject { Espago::ApiConnection.new("http://some.api.example.com") }
 
   context "#create" do
-    it "should return response" do
+    it "returns response" do
       subject.create(:path, :stubbed).should be_a_kind_of Espago::Response
+    end
+    it "raises auth error" do
+      expect { subject.create(:path, :unauthorized) }.to raise_error Espago::AuthenticationError
     end
   end
 end
