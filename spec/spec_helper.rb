25ce08022b4f23b0a5480ec00593193e706054b7
Stanisław Hardt <s.hardt@espago.com>
2022-06-12 01:31:37 +0200

    delete climate, update Espago.api_version

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,25 +7,21 @@
 require 'simplecov'
 SimpleCov.start
 
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-
 require 'webmock/rspec'
 require 'vcr'
 require './lib/espago'
+require 'byebug'
 
 VCR.configure do |c|
   c.default_cassette_options = {:record => :once, :serialize_with => :yaml}
   c.cassette_library_dir = 'spec/fixtures/cassettes'
   c.hook_into :webmock
-  c.ignore_hosts 'codeclimate.com'
 end
 #WebMock.allow_net_connect!
 #VCR.turn_off!(:ignore_cassettes => false)
 #VCR.turn_on!
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
@@ -35,7 +31,7 @@ RSpec.configure do |config|
     Espago.app_password = 'app_password_test'
     Espago.public_key = 'app_public_key_test'
     Espago.production = false
-    Espago.api_version = '2'
+    Espago.api_version = '3'
   end
 
 
